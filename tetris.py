4128974dbed4a1cb117f045af7a0110efcd575fc
Ebrahim <Ebra@226.14.238.24.in-addr.arpa.data1-stratum.pa.hwccustomers.com>
2015-02-07 21:19:39 -0500

    You are watching me do this right now.

--- tetris.py
+++ tetris.py
@@ -1,1 +1,18 @@
-print 'Hello, World!'
+#A B C D
+#E F G H
+
+choice = None
+block = {'A':1} #this is Block 
+
+while True:
+    choice = raw_input('1 for playing, 0 for kill: ')
+    if choice == '1':
+        #start game
+        print 1
+        break
+    elif choice == '0':
+        #end game
+        print 0
+        break
+    else:
+        print 'Invalid Input'    
