c3804e3c4cbf042a7726038bcabb898e8a8ff798
Ebrahim <Ebra@226.14.238.24.in-addr.arpa.data1-stratum.pa.hwccustomers.com>
2015-02-07 21:33:44 -0500

    It's alive

--- tetris.py
+++ tetris.py
@@ -1,15 +1,28 @@
 #A B C D
 #E F G H
 
+# make blocks
+# make function that randomly chooses blocks
+
+import time
+
+MOVMENT_CONSTANT = 1
+BOTTOM = -100
+
 choice = None
-block = {'A':1} #this is Block 
+block = {'A':1} #this is Block
+y = 0 # this is the top of the screen 
 
 while True:
     choice = raw_input('1 for playing, 0 for kill: ')
     if choice == '1':
         #start game
-        print 1
+        while y > BOTTOM: 
+            y -= MOVMENT_CONSTANT 
+            print y
+            time.sleep(1)
         break
+        
     elif choice == '0':
         #end game
         print 0
