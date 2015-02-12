23fb5419fa5550d02def2f63373dae3d6fcc36cb
Ebrahim <Ebra@Ebrahims-MacBook-Pro-2.local>
2015-02-11 19:57:56 -0500

    We woke up from bed, we went to calculus (at least on of us did in realsity :p) blah blah blah thenwe imported the left and right controls, which were awesome :) he is telling what to say. NO. installed pyglet clreated window cleared window ran app.run schedualed functions WEE CANN MOVE
    (still trying to fix the y though)

--- tetris.py
+++ tetris.py
@@ -4,28 +4,57 @@
 # make blocks
 # make function that randomly chooses blocks
 
-import time
-
-MOVMENT_CONSTANT = 1
-BOTTOM = -100
-
 choice = None
 block = {'A':1} #this is Block
-y = 0 # this is the top of the screen 
+y = 0 #this is top of screen
+x = 0 #this is center of screen
+
+import time
+import pyglet
+from pyglet.window import key 
+
+MOVMENT_CONSTANT = 1
+BOTTOM = -30
+
+#create a window
+window = pyglet.window.Window()
+
+@window.event
+def on_draw():
+    window.clear()
+
+@window.event
+def on_key_press(symbol, modifiers):
+    global x #FIX
+    if symbol == key.LEFT:
+        x -= MOVMENT_CONSTANT
+	print 'X:', x
+    elif symbol == key.RIGHT:
+	x += MOVMENT_CONSTANT
+	print 'X:', x
+    elif symbol == key.ESCAPE:
+	window.close 
+
+def fall(dt, *args):
+    y = args[0]
+    if y > BOTTOM:
+	y -= MOVMENT_CONSTANT
+	print 'Y:', y
+
+pyglet.clock.schedule_interval(fall, 1, y)
 
 while True:
     choice = raw_input('1 for playing, 0 for kill: ')
     if choice == '1':
         #start game
-        while y > BOTTOM: 
-            y -= MOVMENT_CONSTANT 
-            print y
-            time.sleep(1)
+	x=0
+	pyglet.app.run()
         break
-        
     elif choice == '0':
         #end game
         print 0
         break
     else:
         print 'Invalid Input'    
+
+window.close()
